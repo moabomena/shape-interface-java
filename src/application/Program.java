2b4902e03a4b1176f4e3b4194f233f31eec97c0f
Moab Omena <1moabomena@gmail.com>
2019-11-02 11:29:55 -0300

    Implementation of the solution

---
+++ src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+
+import model.entities.AbstractShape;
+import model.entities.Circle;
+import model.entities.Rectangle;
+import model.enums.Color;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+		AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+		
+		System.out.println("Circle color: " + s1.getColor());
+		System.out.println("Circle area: " + String.format("%.2f",s1.area()));
+		System.out.println("Reactangle color: " + s2.getColor());
+		System.out.println("Reactangle area: " + String.format("%.2f", s2.area()));
+	}
+}
